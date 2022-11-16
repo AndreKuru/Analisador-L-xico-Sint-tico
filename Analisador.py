ff213133a0639b30785ab0ec06e31819f0361192
julien-vaz <54545601+julien-vaz@users.noreply.github.com>
2022-11-16 13:01:27 -0300

    Determinization fully implemented

--- Analisador.py
+++ Analisador.py
@@ -39,42 +39,75 @@ def determinizarAutomato(automato) -> AF:
   # 1 - Varrer transicoes em busca de transicoes pelo simbolo de palavra vazia
 
   # 1.1 - Calcula os e-fechos do automato original
-  estados_novos = [] * automato.estados
+  e_fechos = dict()
   # 1.1.1 - Adiciona o proprio estado ao seu e-fecho 
   for i in range(automato.estados):
     e_fecho = set()
     e_fecho.add(i)
     e_fecho_aux = e_fecho.copy()
-    estados_novos.insert(i, e_fecho_aux)
+    e_fechos[i] = e_fecho_aux
     e_fecho.clear()
 
   # 1.1.2 - Adiciona demais estados alcançados por e-transicoes
   for transicao in automato.transicoes:
     if '&' in transicao:
       e_fecho = set()
-      e_fecho_aux = e_fecho.union(estados_novos[transicao[0]])
-      e_fecho_aux = e_fecho_aux.union(estados_novos[transicao[2]])
-      e_fecho_aux = estados_novos[transicao[0]].union(e_fecho_aux)
-      estados_novos[transicao[0]] = e_fecho_aux
+      e_fecho_aux = e_fecho.union(e_fechos[transicao[0]])
+      e_fecho_aux = e_fecho_aux.union(e_fechos[transicao[2]])
+      e_fecho_aux = e_fechos[transicao[0]].union(e_fecho_aux)
+      e_fechos[transicao[0]] = e_fecho_aux
       e_fecho.clear()
+
+  estados_chaves = list(e_fechos)
+
+  estados_novos = dict()
+  for i in range(automato.estados):
+    if {estados_chaves[i]} == e_fechos[i]:
+      estados_novos[i] = e_fechos[i]
+    else:  
+      estados_novos[i + automato.estados - 1] = e_fechos[i]
+
+  print('ESTADOS NOVOS MAPEADOS')
+  print(estados_novos)
 
   '''1.3 - Define o e-fecho do estado inicial do automato original
         como estado inicial do automato resultante'''
 
-  estado_inicial_novo = list(estados_novos[0])
+  estado_inicial = estados_chaves[0]
 
   '''1.4 - Define os conjuntos de estados que contem algum estado final do automato
         original como estados finais do automato resultante'''
+  
+  estados_finais = dict()
 
-  estados_finais = set()
-
-  for estado in estados_novos:
+  for (index, estado) in estados_novos.items():
     for estado_final in automato.estados_finais:
       if estado_final in estado:
-        estado = frozenset(estado)
-        estados_finais.add(estado)
+        estados_finais[index] = estado
 
-  '''1.5 - As novas transicoes passam a ser a união dos e-fecho de cada transicao do estado novo'''
+  # 1.5 - As novas transicoes passam a ser a união dos e-fecho de cada transicao do estado novo
+
+  alfabeto_aux = automato.alfabeto.copy()
+  transicoes = list()
+  for estado in estados_novos.values():
+    for simbolo in alfabeto_aux:
+      transicoes.append([estado, simbolo, set()])
+
+  for transicao in automato.transicoes:
+    for i in range(len(transicoes)):
+      if transicao[0] in transicoes[i][0] and transicao[1] == transicoes[i][1]:
+        transicoes[i][2] = transicoes[i][2].union(e_fechos[transicao[2]])
+
+  for (index, estado) in estados_novos.items():
+    for transicao in transicoes:
+      if estado == transicao[0]:
+        transicao[0] = index
+      if estado == transicao[2]:
+        transicao[2] = index
+
+  automato_determinizado = AF.AF(len(estados_novos), estado_inicial, list(estados_finais), automato.alfabeto, transicoes)
+  
+  return automato_determinizado
 
   '''
   2 - Se nao
@@ -128,4 +161,5 @@ a1 = lerAF(sys.argv[1])
 #a3 = uniaoAutomato([a1, a2])
 #a3.imprimirAF()
 #lerER(sys.argv[1])
-determinizarAutomato(a1)
+a4 = determinizarAutomato(a1)
+a4.imprimirAF()
