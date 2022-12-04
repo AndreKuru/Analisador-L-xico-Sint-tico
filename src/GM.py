b21e511e4c7d691c6182857869f94ac1180147d9
rianblockserena <ryannblock95@gmail.com>
2022-12-04 00:44:28 -0300

    leitor da Gramatica

---
+++ src/GM.py
@@ -0,0 +1,45 @@
+class GM:
+    def __init__(self, noterminals, terminals, initial, productions) -> None:
+        self.noterminals = noterminals
+        self.terminals   = terminals
+        self.initial     = initial
+        self.productions = productions
+
+
+    def readGM(file):
+        file = open(f'tests/{file}', 'r')
+        file_rows = file.readlines()
+        file_rows = [rows.rstrip("\n") for rows in file_rows]
+        
+
+        initial       = str(file_rows[0][0])  # Producao inicial
+        productions = dict()                  # dicionario contendo todas as producoes
+
+        # codigo responsável por ler a Gramatica e encontrar as producoes
+        for i in range(len(file_rows)):
+            file_one_row = file_rows[i].split(" -> ")
+            productions[file_one_row[0]] = {file_one_row[1]}
+            productions[file_one_row[0]].add(file_one_row[1])
+        
+
+        noterminals    = set(file_rows[row][0] for row in range(0,len(file_rows))) # conjunto de nao terminais da gramática
+
+
+        # parte responsável por encontrar os não terminais da gramática
+        new_fragments = []
+        fragments = []
+        # vare todas os values do dicionario e separa as producoes por "|"
+        for k in productions.keys():
+           fragments += productions[k]
+        for production in fragments:
+           new_fragments += production.split(" | ")
+        fragments = new_fragments
+        new_fragments = []
+        # vare novamente a lista criada anteriormente e separa por todos os terminais para manter
+        # somente os que são não terminais
+        for noterminal in noterminals:
+           for fragment in fragments:
+               new_fragments += fragment.split(noterminal)
+           fragments = new_fragments
+           new_fragments = []
+        terminals = set(fragments)
