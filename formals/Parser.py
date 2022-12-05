aa889c2479541edd71f53756da29fc8964c0a90e
Julien Hervot de Mattos Vaz <jhmvaz95@gmail.com>
2022-12-05 17:18:10 -0300

    Parser's extendGrammar implemented
    	new file:   formals/Parser.py

---
+++ formals/Parser.py
@@ -0,0 +1,73 @@
+from GM import GM
+import sys
+import os
+
+def markProduction(production):
+
+    marked_production = '.' + production
+    
+    return set(marked_production)
+
+
+
+def buildCanonicalItems(grammar):
+    for hbody in grammar.productions.values():
+        unmarked_productions = list(body)
+        marked_productions = list()
+        for production in unmarked_productions:
+            if '.' not in production:
+                marked_production = markProduction(production)
+                marked_productions.append(marked_production)
+
+def extendGrammar(grammar):
+
+    initial = grammar.initial + "'"
+    noterminals = grammar.noterminals
+    terminals = grammar.terminals
+    productions = grammar.productions
+    productions[initial] = grammar.initial
+
+    extended_gm = GM(noterminals, terminals, initial, productions)
+
+    return extended_gm
+
+def generateSLRParser(grammar):
+
+    # Estender gramática
+    extended_gm = extendGrammar(grammar)
+
+    # Construir itens canônicos (automato)
+    canonical_items = buildCanonicalItems(extended_gm)
+
+    # Contruir tabela SLR
+    SLRTableTerminals = buildSLRTableTerminals(canonical_items, extended_gm)
+    SLRTableNonTerminals = buildSLRTableNonTerminals(canonical_items, extended_gm)
+     
+        # Marcar os shifts
+        markShifts(SLRTableTerminals)
+
+        # Marcar os reduces
+        markReduces(SLRTableTerminals)
+
+        # Marcar o accept
+        markAccept(SLRTableTerminals)
+
+        # Calcular os follows
+        follows = calculateFollows(extended_gm)
+
+        # Marcar os desvios
+        markGoTos(SLRTableNonTerminals)
+
+        # Demais entradas na tabela são erros
+
+        # Estado inicial é o que contém a produção pelo símbolo inicial da gramática estendida
+
+noterminals = {'E', 'T', 'F'}
+terminals = {'+', '*', '(', ')', 'id'}
+initial = 'E'
+productions = {'E': {'E+T', 'T'},
+               'T': {'T*F', 'F'},
+               'F': {'(E)', 'id'}}
+
+gm = GM(noterminals, terminals, initial, productions)
+generateSLRParser(gm)
