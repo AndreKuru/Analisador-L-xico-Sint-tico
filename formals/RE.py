6739b4b70a9b1ee5e56bd0045005f592374a4814
andrekuru <andrewilliam13@gmail.com>
2022-12-05 23:12:50 -0300

    reformated code and added token 	modified:   formals/RE.py 	modified:   formals/Tree.py

--- formals/RE.py
+++ formals/RE.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass, field
 from ast import operand
 from cmath import exp
 from enum import Enum
@@ -24,55 +25,33 @@ def getType(char):
     print("ERROR")
 
 
-def generateTree(expression):
-
-    # O primeiro símbolo tem de ser um operador
-    currentType = getType(expression[0])
-    if currentType != ExpressionType.operand:
-        print("ERROR")
-
-    # Inicializa a árvore
-    tree = BinaryTree(expression[0])
-
-    # Insere todos os símbolos da expressão na árvore
-    for i in range(1, len(expression)):
-        e = expression[i]
-
-        lastType = currentType
-        currentType = getType(e)
-
-        if lastType == ExpressionType.binaryOperation:
-            if currentType == ExpressionType.operand:
-                tree.insertRight(e)
-            else:
-                print("ERROR")
-        elif currentType == ExpressionType.operand:
-            tree.insertAbove("·")
-            tree.insertRight(e)
-        else:
-            tree.insertAbove(e)
-
-    tree.fillTree
-
-
+@dataclass
 class RE:
-    def __init__(self, definitions, expressions) -> None:
-        self.definitions = definitions
-        self.expressions = expressions
-        self.expressionsInUse = set()
-        self.expressionsUntouched = set(definitions)
+
+    definitions: list[tuple[str, str]]
+    expressionsInUse: set[str] = field(default_factory=set)
+    expressionsUntouched: set[str] = field(default_factory=set)
+
+    def getDefinition(self, index):
+        return self.definitions[index][0]
+
+    def getExpression(self, index):
+        return self.definitions[index][1]
+
+    def setExpression(self, index, new_expression):
+        self.definitions[index][1] = new_expression
 
     def openExpression(self, index):
-        if self.expressions[index] in self.expressionsUntouched:
-            self.expressionsUntouched.remove(self.expressions[index])
+        if self.getDefinition(index) in self.expressionsUntouched:
+            self.expressionsUntouched.remove(self.getDefinition(index))
 
-        if self.expressions[index] in self.expressionsInUse:
+        if self.getDefinition(index) in self.expressionsInUse:
             print("Error")
-        self.expressionsInUse.append(self.expressions[index])
+        self.expressionsInUse.add(self.getDefinition(index))
 
         fragments = list()
         for i in range(len(self.definitions)):
-            fragments = self.expressions[index].split(self.definitions[i])
+            fragments = self.definitions[index][1].split(self.definitions[i])
 
             if len(fragments):
                 if self.definitions[i] in self.expressionsInUse:
@@ -88,25 +67,63 @@ class RE:
                     newExpression.append(self.expressions[index][i])
                     newExpression.append(fragments[j])
 
-                self.expressions[index] = "".join(newExpression)
+                self.setExpression("".join(newExpression))
 
         self.expressionsInUse.remove(self.expressions[index])
 
+    def generateTree(self, expression_index) -> BinaryTree:
+
+        expression = self.getExpression(expression_index)
+        token = self.getDefinition(expression_index)
+
+        # O primeiro símbolo tem de ser um operador
+        currentType = getType(expression[0])
+        if currentType != ExpressionType.operand:
+            print("ERROR")
+
+        # Inicializa a árvore
+        tree = BinaryTree(token, expression[0])
+
+        # Insere todos os símbolos da expressão na árvore
+        for i in range(1, len(expression)):
+            e = expression[i]
+
+            lastType = currentType
+            currentType = getType(e)
+
+            if lastType == ExpressionType.binaryOperation:
+                if currentType == ExpressionType.operand:
+                    tree.insertRight(e)
+                else:
+                    print("ERROR")
+            elif currentType == ExpressionType.operand:
+                tree.insertAbove("·")
+                tree.insertRight(e)
+            else:
+                tree.insertAbove(e)
+
+        tree.fillTree
+
     def generateFAs(self):
 
+        # Inicializa com todas as definições marcadas como intactas
+        expresionsUntouched = set()
+        for definition, _ in self.definitions:
+            expresionsUntouched.add(definition[0])
+
         # Percorre todas as expressões (ignorando as já abertas)
-        for i in range(len(self.expressions)):
-            if self.expressions[i] in self.expressionsUntouched:
+        for i in range(len(self.definitions)):
+            if self.definitions[i][0] in self.expressionsUntouched:
                 self.openExpression(i)  # self.openExpression(i)
 
         self.trees = list()
         # Gera a árvore de cada expressão
-        for expression in self.expressions:
-            self.trees.append(generateTree(expression))
+        for expression_index in range(len(self.definitions)):
+            self.trees.append(self.generateTree(expression_index))
 
         finite_automatas = list()
         # Gera o autômato de cada árvore
         for tree in self.trees:
-            finite_automatas.add(tree.generateFA())
+            finite_automatas.append(tree.generateFA())
 
         return finite_automatas
